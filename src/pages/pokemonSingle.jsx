8b44d2287e76f6da02fda2f5b1ea04f92ee6bf7c
Lucas <lucasbombaron@gmail.com>
2024-03-29 11:20:48 +0100

    pokemon page

--- src/pages/pokemonSingle.jsx
+++ src/pages/pokemonSingle.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { PokemonContext } from "../data/pokeContext";
 
 
 function PokemonPage() {
-    const {id} = useParams()
+    const data = useContext(PokemonContext);
+    const {id} = useParams();
+
+    const pokemon = data[id - 1];
+    const imgPath = pokemon.sprites.regular
+
+    console.log(pokemon);
 
     return(
-        <h1>pokemon {id}</h1>
+        <div>
+            <img src={imgPath} alt={pokemon.name.fr} />
+            <h1>{pokemon.name.fr}</h1>
+        </div>
     )
 }
 
