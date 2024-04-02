fa9a258fcc91145fc29edb48150e36516b2d33d2
Lucas <lucasbombaron@gmail.com>
2024-04-02 17:10:05 +0200

    pokeType size props

--- src/components/pokeType.jsx
+++ src/components/pokeType.jsx
@@ -2,10 +2,13 @@ function PokeType(props) {
 
     const type = props.type.name;
     const iconUrl = require(`../assets/type/${type}.svg`);
+    let sizeType;
+    if (props.size === "small") sizeType = 'h-5';
+    if (props.size === "large") sizeType = 'h-6';
 
     return(
         <>
-            <img src={iconUrl} alt={type} className="h-5" />
+            <img src={iconUrl} alt={type} className={sizeType} />
             {/* <p>{props.type.name}</p> */}
         </>
     )
