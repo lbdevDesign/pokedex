238e4545bf6e3c87a5124a36727174087670df8a
Lucas <lucasbombaron@gmail.com>
2024-04-02 15:20:00 +0200

    responsive home

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
 
   return (
     <div className="flex flex-col items-center bg-[url('./assets/forest.jpeg')] ">
-      <div className="w-11/12 bg-white p-12 max-w-7xl flex justify-center rounded-b-lg">
-        <img src={logo} alt="Logo" className="h-44" />
+      <div className="w-11/12 bg-white p-4 max-w-7xl flex justify-center rounded-b-lg">
+        <img src={logo} alt="Logo" className="h-32" />
       </div>
-      <div className="grid grid-cols-3 gap-8 w-11/12 max-w-7xl my-8">
+      <div className="grid grid-cols-1 gap-8 w-11/12 max-w-7xl my-8 min-[768px]:grid-cols-2 min-[1140px]:grid-cols-3 ">
         {pokemons.map((pokemon, index) => (
             <PokeCard key={index} data={pokemon} />
         ))}
